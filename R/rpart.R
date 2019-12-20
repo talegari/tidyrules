5ea801ca1d7a167ebdd733c11a49acbf59a2a57b
uramith <uramith@gmail.com>
2019-12-20 21:04:48 +0530

    Rpart regression added (#12)
    * rpart scripts added
    
    * example changed and update dependency list
    
    * dplyr dependency removed, doc updated
    
    * rpart clean
    
    * Vignette added along with some changes
    
    * Vignette final
    
    * added rpart regression and tests for the same

--- R/rpart.R
+++ R/rpart.R
@@ -24,6 +24,7 @@ tidyRules.rpart <- function(object, ...){
   # check for rpart object
   stopifnot(inherits(object, "rpart"))
 
+  if(object$method == "class"){
   # Stop if only root node is present in the object
   if(nrow(object$frame) == 1){
     stop(paste0("Only root is present in the rpart object"
@@ -119,6 +120,80 @@ tidyRules.rpart <- function(object, ...){
     tibble::as_tibble()
 
   return(tidy_rules)
+  }
+  else{
+    # Stop if only root node is present in the object
+    if(nrow(object$frame) == 1){
+      stop(paste0("Only root is present in the rpart object"
+
+      )
+      )
+    }
+
+    # Stop if any ordered factor is present:
+    # partykit doesn't handle the ordered factors while processing rules.
+    if(sum(object$ordered) > 0){
+      stop(paste0("Ordered variables detected!!"
+                  , "convert ordered variables"
+                  , " to factor or numberic before model fit"))
+    }
+
+    # column names from the object: This will be used at the end to handle the
+    # variables with a space.
+    col_names <- stringr::str_remove_all(attr(object$terms,which = "term.labels")
+                                         , pattern = "`")
+
+    # throw error if there are consecutive spaces in the column names ----
+    if(any(stringr::str_count(col_names, "  ") > 0)){
+      stop("Variable names should not have two or more consecutive spaces.")
+    }
+
+    # convert to class "party"
+    party_obj <- partykit::as.party(object)
+
+    # extracting rules
+    rules <- list.rules.party(party_obj) %>%
+      stringr::str_replace_all(pattern = "\\\"","'") %>%
+      stringr::str_remove_all(pattern = ", 'NA'") %>%
+      stringr::str_remove_all(pattern = "'NA',") %>%
+      stringr::str_remove_all(pattern = "'NA'") %>%
+      stringr::str_squish()
+
+    # terminal nodes from party object
+    terminal_nodes <- partykit::nodeids(party_obj, terminal = T)
+
+    # extract metrics from rpart object
+    metrics <- object$frame[terminal_nodes,c("n","dev","yval")]
+    # metrics$confidence <- (metrics$n + 1 - metrics$dev) / (metrics$n + 2)
+
+    metrics <- metrics[,c("n","yval")] %>%
+      magrittr::set_colnames(c("support","RHS"))
+
+    metrics$LHS <- rules
+
+    tidy_rules <- metrics
+
+    # replace variable names with spaces within backquotes ----
+    for(i in 1:length(col_names)){
+      tidy_rules[["LHS"]] <- stringr::str_replace_all(
+        tidy_rules[["LHS"]]
+        , col_names[i]
+        , addBackquotes(col_names[i])
+      )
+    }
+
+    # return ----
+    tidy_rules <- tibble::rowid_to_column(tidy_rules, "id")
+    tidy_rules <- tidy_rules[, c("id"
+                                 , "LHS"
+                                 , "RHS"
+                                 , "support"
+    )
+    ] %>%
+      tibble::as_tibble()
+
+    return(tidy_rules)
+    }
 
 }
 
